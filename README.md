4c488435463da3c746cd041983290f4ac5494e2b
Padhu Ramalingam <apadhu@yahoo.com>
2026-02-06 22:34:33 -0500

    first commit. Completed upto testing section

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-graphql
+Work are for Spring GraphQL tutorial at https://spring.io/guides/topicals/observing-graphql-in-action.
---
+++ src/main/java/io/spring/guides/graphqlmusic/tracks/TrackRepository.java
@@ -0,0 +1,10 @@
+package io.spring.guides.graphqlmusic.tracks;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+
+public interface TrackRepository extends MongoRepository<Track, String> {
+    List<Track> findByAlbumIdOrderByNumber(String albumId);
+}
